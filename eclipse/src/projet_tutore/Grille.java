6e268fda1c2de178dbec8c24b334eec8763a8b4c
alain1996 <alain-92@hotmail.fr>
2015-05-12 19:37:07 +0200

    Commit
    j’ai mis a jour les fichier, maintenant on prendre en compte
    l’affichage de la grille et de la test de la grille

--- eclipse/src/projet_tutore/Grille.java
+++ eclipse/src/projet_tutore/Grille.java
@@ -1,37 +1,81 @@
-package projet_tutore;
-
 public class Grille {
    
 	// declaration des variables
     private int tailleAbscisse;
-
     private int tailleOrdonnee;
+    private char[] carte;
  
     // constructeur par défaut
-    
     public Grille() { 
-    	this.tailleAbscisse = 0;
-    	this.tailleOrdonnee = 0;
-    } 
-    
+    	this.tailleAbscisse = 50;
+    	this.tailleOrdonnee = 20;
+    	this.carte = new char[400];
+    }
+   
     // construteur champs à champs
-    public Grille(int tailleAbscisse, int tailleOrdonne){
-        
+    public Grille(int tailleAbscisse, int tailleOrdonnee, char[] carte){
     	this.tailleAbscisse = tailleAbscisse;
-    	this.tailleOrdonnee =tailleOrdonnee;
+    	this.tailleOrdonnee = tailleOrdonnee;
+    	this.carte = carte;
     }
     
     public void initGrille() {
+    	this.carte= new char[this.tailleAbscisse*this.tailleOrdonnee];
+    	for(int i=0;i<this.tailleAbscisse*this.tailleOrdonnee;i++){
+    		this.carte[i]=' ';
+    	}
+    	for(int i=0;i<this.tailleAbscisse;i++){
+    		this.carte[i]='#';
+    	}
+    	for(int i=(this.tailleOrdonnee-1)*this.tailleAbscisse;i<this.tailleOrdonnee*this.tailleAbscisse;i++){
+    		this.carte[i]='#';
+    	}
+    	for(int i=0;i<this.tailleOrdonnee*this.tailleAbscisse;i+=this.tailleAbscisse){
+    		this.carte[i]='#';
+    	}
+    	for(int i=this.tailleAbscisse-1;i<this.tailleOrdonnee*this.tailleAbscisse;i+=this.tailleAbscisse){
+    		this.carte[i]='#';
+    	}
     }
 
-
-    public void affichage() {
+    public boolean caseValide(int x, int y){
+    	if(x>=0 && x<this.tailleAbscisse && y>=0 && y<this.tailleOrdonnee){
+    		return(true);
+    	}
+    	else{
+    		return(false);
+    	}
     }
 
+    public void ajoutMur(int x, int y){
+    	if(caseValide(x,y)){
+    		this.carte[x+y*this.tailleAbscisse]='#';
+    	}
+    }
+    
+    public int valeurCase(int x, int y){
+    	if(caseValide(x,y)){
+    		return(this.carte[x+this.tailleAbscisse*y]);
+    	}
+    	else{ 
+    		return(-2);
+    	}
+    }
 
-    public boolean caseVide (int abscisse, int ordonnee) {
-    	
-    	return false;
+    public void modifierCase(int x, int y, char valeur){
+    	if(valeurCase(x,y) != -2){
+    		this.carte[x+this.tailleAbscisse*y] = valeur;
+    	}
+    }
+    
+    public void affichage(){
+    	String Newligne=System.getProperty("line.separator");
+    	for(int i=0;i<this.tailleOrdonnee;i++){
+    		for(int j=0;j<this.tailleAbscisse;j++){
+    			System.out.print(this.carte[j+i*this.tailleAbscisse]);
+    		}
+    		System.out.print(Newligne);
+    	}
     }
 
 // methode d'acces
@@ -46,16 +90,22 @@ public class Grille {
         this.tailleAbscisse = value;
     }
 
-
     int getTailleOrdonnee() {
         // Automatically generated method. Please delete this comment before entering specific code.
         return this.tailleOrdonnee;
     }
 
-
     void setTailleOrdonnee(int value) {
         // Automatically generated method. Please delete this comment before entering specific code.
         this.tailleOrdonnee = value;
     }
+    
+    char[] getCarte() {
+    	return this.carte;
+    }
 
+    void setCarte(char[] value){
+    	this.carte = value;
+    }
+    
 }
